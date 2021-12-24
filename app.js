8cb214ee44b75ea041e673a13287a0cec13e3ea0
shivrajbande <shivabande09@gmail.com>
2021-12-24 15:18:45 +0530

    port changed

--- app.js
+++ app.js
@@ -383,7 +383,7 @@ else{
  res.render("winner",{records:data});
 })
 
-let port = process.env.port||3000;
+let port = process.env.PORT||3000;
 // if(port==null || port==""){
 //     port = 3000;
 // }
